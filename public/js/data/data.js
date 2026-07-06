6c01473bb3e7f354d5400e19d1e93deb4a5a7a28
abraaosantosdev <abraaofilipi12@gmail.com>
2026-07-06 15:05:16 -0300

    Updated folders

---
+++ public/js/data/data.js
@@ -0,0 +1,50 @@
+users: [
+    {
+        id: 1,
+        nome: "Abraão Santos",
+        email: "abraao@email.com",
+        apelido: "afs_dev26"
+    },
+    {
+        id: 2,
+        nome: "Luna Valen",
+        email: "luna.valen@fantasia.dev",
+        apelido: "moonweaver"
+    },
+    {
+        id: 3,
+        nome: "Kael Draven",
+        email: "kael.draven@fantasia.dev",
+        apelido: "bladeborn"
+    },
+    {
+        id: 4,
+        nome: "Mira Solstice",
+        email: "mira.solstice@fantasia.dev",
+        apelido: "sunseer"
+    },
+    {
+        id: 5,
+        nome: "Thorne Eldor",
+        email: "thorne.eldor@fantasia.dev",
+        apelido: "stonewarden"
+    },
+    {
+        id: 6,
+        nome: "Nyla Frost",
+        email: "nyla.frost@fantasia.dev",
+        apelido: "icewhisper"
+    },
+    {
+        id: 7,
+        nome: "Orin Vale",
+        email: "orin.vale@fantasia.dev",
+        apelido: "starlight"
+    },
+    {
+        id: 8,
+        nome: "Seraphine Moss",
+        email: "seraphine.moss@fantasia.dev",
+        apelido: "wildbloom"
+    }
+]
